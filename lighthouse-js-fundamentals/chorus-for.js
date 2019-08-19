222c37ce8e591dbd8432139482c16a1d28da9857
Samir Parab <samirparab@yahoo.com>
2019-08-19 12:52:45 +0000

    Use a for loop to repeat a chorus line

---
+++ lighthouse-js-fundamentals/chorus-for.js
@@ -0,0 +1,7 @@
+// common structure for looping in JavaScript is the for loop. The idea of a for loop is very similar to a while, only all the code that relates to the state of the loop is written on one line.
+
+const chorus = "Let's dance!";
+for (let repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+console.log("Until the sun comes up!");
