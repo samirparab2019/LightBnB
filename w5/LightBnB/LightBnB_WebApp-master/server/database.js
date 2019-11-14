c5ab5c8d77ded0bbe5e1f61dedc6b112c527b8c2
Samir Parab <samirparab@yahoo.com>
2019-11-14 21:01:30 +0000

    add property function updated with sql query.

--- w5/LightBnB/LightBnB_WebApp-master/server/database.js
+++ w5/LightBnB/LightBnB_WebApp-master/server/database.js
@@ -136,7 +136,7 @@ const getAllProperties = function(options, limit = 10) {
     queryString += ` HAVING avg(rating) >= $${queryParams.length}`;
   }
   queryParams.push(limit);
-  queryString +=  ` ORDER BY cost_per_night
+  queryString += ` ORDER BY cost_per_night
   LIMIT $${queryParams.length};
   `;
 
@@ -150,24 +150,26 @@ const getAllProperties = function(options, limit = 10) {
 
 exports.getAllProperties = getAllProperties;
 
-
-
-
-
-
-
-
-
-
 /**
  * Add a property to the database
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
+
+  return pool.query(`INSERT INTO properties 
+  (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code])
+  
+  .then(res => res.rows);
+
 }
 exports.addProperty = addProperty;
+
+
+
+
